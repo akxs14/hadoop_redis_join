7d3df9af0aa1ea9f72bddfb5bb2b1aaa3169bf0a
angelos <angelos.kapsimanis@madvertise.com>
2014-04-17 11:50:38 +0200

    made the script more readable

--- clicks_impressions_join.rb
+++ clicks_impressions_join.rb
@@ -10,6 +10,21 @@ def read_file filename
   end
 end
 
+def is_impression? event
+  event["ais"] == 1
+end
+
+def resulted_in_click? redis, event
+  redis.hget("clicks", event["tx"]) == event["tx"]
+end
+
+def match_ai_click redis, event
+  if resulted_in_click?(redis, event)
+    print "."
+    event["clicks"] = 1
+  end
+end
+
 redis = Redis.new(:host => "localhost", :port => 6379, :db => 1)
 
 puts "store clicks in redis"
@@ -26,14 +41,8 @@ out_file = File.open("clicks_impressions.txt", "w")
 puts "\nstore clicks in redis"
 read_file("historical_test_file") do |line|
   parsed_line = JSON.parse(line)
-
-  # How to avoid double-counting?
-  if parsed_line["ais"] == 1
-  	if redis.hget("clicks", parsed_line["tx"]) == parsed_line["tx"]
-    	print "."
-    	parsed_line["clicks"] = 1
-    end
-
-    out_file.write(parsed_line.to_s + "\n")
+  if is_impression?(parsed_line)
+    out_file.write(match_ai_click(redis,parsed_line).to_s + "\n")
   end
 end
+
