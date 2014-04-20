29655a2f6a08a4ba46ccf4e1778ce4116b59eedd
Hostmaster of the day <root@localhost>
2014-04-20 22:48:08 +0200

    working script

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#hadoop jar build/hadoop_redis_join.jar collect_ids "`hadoop fs -cat /events/info.json | ruby logs_between.rb -2.weeks +10.minute`" /tmp/angelos/id
 hadoop fs -cat /events/info.json | ruby logs_between.rb -2.weeks +1.day | time hadoop jar build/hadoop_redis_join.jar collect_ids
